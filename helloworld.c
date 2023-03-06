bcbbc2fea8c332e37e364b4983c7240b3bb0c19e
Daniel Ortiz <thisisdanielortiz@gmail.com>
2023-03-06 13:25:31 -0600

    first commit

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// This is a comment
+int main() {
+  printf("Hello World!");
+  /* This is a multiline comment
+  here you can write a lot of text en C will
+  not take it to count
+  */
+ printf("This is my first line of code in C");
+
+return 0;
+}
+
